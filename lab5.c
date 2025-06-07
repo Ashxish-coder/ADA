56f880ac8e8076dd35624d6a698e6b6d201fcea3
Ashxish-coder <ashishkinagi@gmail.com>
2025-06-08 00:39:17 +0530

    Update lab5.c

--- lab5.c
+++ lab5.c
@@ -1,47 +1,43 @@
-#include<stdio.h>
-#include<conio.h>
-int temp[10],k=0;
-void sort(int a[][10],int id[],int n)
-{
-    int i,j;
-    for(i=1; i<=n; i++)
-    {
-        if(id[i]==0)
-        {
-            id[i]=-1;
-            temp[++k]=i;
-            for(j=1; j<=n; j++)
-            {
-                if(a[i][j]==1 && id[j]!=-1)
-                    id[j]--;
-            }
-            i=0;
-        }
-    }
-}
-void main()
-{
-    int a[10][10],id[10],n,i,j;
-    printf("\nEnter the n value:");
-    scanf("%d",&n);
-    for(i=1; i<=n; i++)
-        id[i]=0;
-    printf("\nEnter the graph data:\n");
-    for(i=1; i<=n; i++)
-        for(j=1; j<=n; j++)
-        {
-            scanf("%d",&a[i][j]);
-            if(a[i][j]==1)
-                id[j]++;
-        }
-    sort(a,id,n);
-    if(k!=n)
-        printf("\nTopological ordering not possible");
-    else
-    {
-        printf("\nTopological ordering is:");
-        for(i=1; i<=k; i++)
-            printf("%d ",temp[i]);
-    }
-    getch();
-}
+#include<stdio.h> 
+ 
+int cost[10][10],n,colsum[10]; 
+ 
+void cal_colsum() 
+{ 
+ for(int j=0;j<n;j++) 
+ { 
+  colsum[j]=0; 
+  for(int i=0;i<n;i++) 
+   colsum[j]+=cost[i][j]; 
+ } 
+} 
+ 
+void source_removal() 
+{ 
+ int i,j,k,select[10]={0}; 
+ printf("Topological ordering is:"); 
+ for(i=0;i<n;i++) 
+ { 
+  cal_colsum(); 
+  for(j=0;j<n;j++) 
+  {  
+   if(select[j]==0 && colsum[j]==0)
+    break; 
+  } 
+  printf("%d ",j); 
+  select[j]=1; 
+  for(k=0;k<n;k++) 
+   cost[j][k]=0; 
+ } 
+} 
+ 
+void main()  
+{ 
+    printf("Enter no. of Vertices: "); 
+    scanf("%d",&n); 
+    printf("Enter the cost matrix\n"); 
+    for(int i=0;i<n;i++) 
+        for(int j=0;j<n;j++) 
+            scanf("%d",&cost[i][j]); 
+    source_removal(); 
+} 
